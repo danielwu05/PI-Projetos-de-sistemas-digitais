f13d1e250c38c35d6c1880d535efd38ecbf93842
Daniel Wu <danielwu288@gmail.com>
2025-09-24 17:20:04 -0300

    added random toppings method

--- IdeaProjects/Pizza Game/src/ShuffleToppings.java
+++ IdeaProjects/Pizza Game/src/ShuffleToppings.java
@@ -1,24 +1,14 @@
 import java.util.*;
-
-
 public class ShuffleToppings {
-
     public static void main(String[] args) {
         List<Toppings> pizzaToppings = new ArrayList<>(Arrays.asList(Toppings.values()));
         Set<Toppings> pizzaOrder= EnumSet.noneOf(Toppings.class);
         Random rand = new Random();
         int i;
-        
         Collections.shuffle(pizzaToppings);
-
         for ( i= 0; i < 6; i ++) {
             pizzaOrder.add(pizzaToppings.get(i));
         }
-
         System.out.println(pizzaOrder);
-
     }
-
-
-
 }
